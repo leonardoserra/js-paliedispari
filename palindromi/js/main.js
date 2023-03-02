c4d62478f8aaf850522cda1186d6d69f52fd45fc
leonardoserra <serra.leonardo92@gmail.com>
2023-03-02 16:21:54 +0100

    finito

--- palindromi/js/main.js
+++ palindromi/js/main.js
@@ -4,3 +4,34 @@
         Creare una funzione per capire se la 
         parola inserita è palindroma
 */
+
+const userWord = prompt('scrivi qualcosa, vediamo un po\' se è palindroma:');
+
+let result = isItPalindrom(userWord);
+console.log(result);
+
+function isItPalindrom (word){
+
+    let defaultWord = "";
+    let reverseWord = "";
+
+    for(i = 0; i < word.length; i++){
+
+        defaultWord += word[i];
+        console.log('default: ' + defaultWord);
+        
+    }
+
+    for(i = word.length - 1 ; i >= 0; i--){
+
+        reverseWord += word[i];
+        console.log('reverse: ' + reverseWord)
+    }
+
+    if(defaultWord == reverseWord){
+        return "la parola che hai scelto è palindroma";
+    } else{
+        return "la parola che hai scelto non è palindroma";
+    }
+
+}
