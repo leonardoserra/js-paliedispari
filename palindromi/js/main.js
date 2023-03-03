9d03034d9d980aa54feee642047f5e394e9fe808
leonardoserra <serra.leonardo92@gmail.com>
2023-03-03 09:46:01 +0100

    pulito il codice tolta defaultWord e ciclo inutili

--- palindromi/js/main.js
+++ palindromi/js/main.js
@@ -6,21 +6,14 @@
 */
 
 const userWord = prompt('scrivi qualcosa, vediamo un po\' se è palindroma:');
-
+console.log('hai scelto: ' + userWord + ' vediamo se al contrario è uguale');
 let result = isItPalindrom(userWord);
 console.log(result);
 
 function isItPalindrom (word){
 
-    let defaultWord = "";
+
     let reverseWord = "";
-
-    for(i = 0; i < word.length; i++){
-
-        defaultWord += word[i].trimStart();
-        console.log('default: ' + defaultWord);
-        
-    }
 
     for(i = word.length - 1 ; i >= 0; i--){
 
@@ -28,7 +21,7 @@ function isItPalindrom (word){
         console.log('reverse: ' + reverseWord)
     }
 
-    if(defaultWord == reverseWord){
+    if(word == reverseWord){
         return "la parola che hai scelto è palindroma";
     } else{
         return "la parola che hai scelto non è palindroma";
